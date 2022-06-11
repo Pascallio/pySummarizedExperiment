51d6810955231c1869c90202084a4773ae5d4446
Pascal <p.maas92@gmail.com>
2022-06-11 21:24:07 +0200

    pip update

---
+++ pySummarizedExperiment/init.py
@@ -0,0 +1,9 @@
+"""
+pySummarizedExperiment.
+
+Python port of R's SummarizedExperiment package.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Pascal Maas'
+__credits__ = 'Martin Morgan, Valerie Obenchain, Jim Hester, Hervé Pagès'
